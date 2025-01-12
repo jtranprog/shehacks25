0a80921fc5952324e5ddb69a0f4435e97ba0e7f2
Juna <115675811+junakim0118@users.noreply.github.com>
2025-01-12 00:37:08 -0500

    components: blind box and progress bars done

--- shehacks/src/App.js
+++ shehacks/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import BlindBox from './components/blind-box';
+import ProgressBar from './components/progressbar';
+
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
         <img src={'./images/logo.png'} className="App-logo" alt="logo" />
         <div className='blindbox'>
         <BlindBox /></div>
+        <div className='progressbar'>
+        <ProgressBar /></div>
     </div>
   );
 }
---
+++ shehacks/src/components/progressbar.js
@@ -0,0 +1,21 @@
+import "./progressbar.css";
+import React, {useState} from 'react';
+
+const ProgressBar = () => {
+    let progress = 2/4;
+    const linewidth = {
+        width: (progress*500),
+    }
+
+  return (
+    <div className="barbody">
+    <div className="progress-bar">
+  <div className="label" data-count={progress*100}>{progress*100}%</div>
+  <div className="line">
+    <span style={linewidth}></span>
+  </div></div>
+</div>
+  );
+};
+
+export default ProgressBar;
